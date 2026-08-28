17ea227d91fe5350fe02a64776d665510e17b643
masashitakano917-maker <masashitakano917@gmail.com>
2026-08-28 21:23:42 +0900

    Remove services from shared footer

--- bigsum_site/site.js
+++ bigsum_site/site.js
@@ -18,7 +18,6 @@ const siteFooter = `
     <div class="footer-inner">
       <div class="footer-brand"><a class="logo" href="./index.html"><img class="logo-image" src="./assets/bigsum_logo.png" alt="BIGSUM" width="2253" height="342" /></a><p>モノを届けるだけではなく、人と人、想いと未来をつなぐ<br>ラストワンマイルのパートナーです。</p></div>
       <div class="footer-column"><h3>私たちについて</h3><a href="./about.html#company">会社概要</a><a href="./about.html#message">代表メッセージ</a><a href="./about.html#philosophy">企業理念</a><a href="./about.html#access">アクセス</a></div>
-      <div class="footer-column"><h3>サービス</h3><a href="./services.html#business">法人配送</a><a href="./services.html#personal">個人配送</a><a href="./services.html#spot">スポット配送</a><a href="./services.html#charter">チャーター便</a></div>
       <div class="footer-column"><h3>採用情報</h3><a href="./recruit.html#jobs">募集職種</a><a href="./recruit.html#environment">働く環境</a><a href="./people.html#interviews">先輩インタビュー</a><a href="./recruit.html#faq">よくある質問</a></div>
       <div class="footer-column"><h3>宅配ボックス購入</h3><a href="./box.html#about">BIGSUM BOXとは</a><a href="./box.html#products">商品一覧</a><a href="./box.html#guide">ご利用ガイド</a><a href="./box.html#faq">よくある質問</a></div>
       <div class="footer-column"><h3>お問い合わせ</h3><a href="./contact.html">お問い合わせフォーム</a></div>
