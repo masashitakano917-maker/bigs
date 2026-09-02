fc9093a976c1688093ce245e146990964e0746bb
masashitakano917-maker <masashitakano917@gmail.com>
2026-09-02 20:46:00 +0900

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   root: ".",
   publicDir: "assets",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        box: resolve(__dirname, "box.html"),
+        contact: resolve(__dirname, "contact.html"),
+        legal: resolve(__dirname, "legal.html"),
+        news: resolve(__dirname, "news.html"),
+        privacy: resolve(__dirname, "privacy.html"),
+        product: resolve(__dirname, "product.html"),
+        recruit: resolve(__dirname, "recruit.html"),
+        services: resolve(__dirname, "services.html"),
+      },
+    },
   },
   server: {
     port: 5173,
